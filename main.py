4d66aed83d9b6279844a16e9a6fec5b70c295f00
jerinjv32 <jerinjvarghese32@gmail.com>
2025-12-31 01:01:14 +0530

    initial Commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from huggingface_hub import InferenceClient
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+load_dotenv()
+
+class PromptModel(BaseModel):
+    prompt: str
+
+class ResponseModel(BaseModel):
+    title: str
+
+HF_TOKEN = os.getenv('HF_TOKEN')
+
+client = InferenceClient(
+    api_key = HF_TOKEN,
+)
+
+@app.post('/chat')
+def chat_bot(body: PromptModel):
+    prompt = body.prompt
+    completion = client.chat.completions.create(
+        model="meta-llama/Llama-3.1-8B-Instruct:novita",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+    )
+    response = completion.choices[0].message.content
+    return ResponseModel(title=response)
+
